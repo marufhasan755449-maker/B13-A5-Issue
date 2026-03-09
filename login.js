d037f561932bfb7318a603f60fb50d4c588dc0f5
unknown <you@example.com>
2026-03-09 13:24:30 +0600

    my website

---
+++ login.js
@@ -0,0 +1,17 @@
+console.log("my name is maruf")
+document.getElementById("Sign-btn").addEventListener("click",function(){
+    const inputName = document.getElementById("input-name");
+    const UserName = inputName.value;
+    
+    const inputPin =document.getElementById("input-pin");
+    const password = inputPin.value;
+
+    if(UserName==="admin" && password==="admin123"){
+        alert("Sign In Success");
+        window.location.assign("/home.html")
+    }
+    else{
+        alert("Sign In Failed");
+        return;
+    }
+})
